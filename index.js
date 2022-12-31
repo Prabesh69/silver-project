589f437143c2ef6e3917f3f87e390d29704196b6
unknown <prabesh037@gmail.com>
2022-12-31 09:50:37 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let b = document.getElementsByClassName("btn");
+function go(){
+    window.open('https://drive.google.com/drive/folders/1Z3AgWU4EsMBxmfUVqBVQJJuAxNoFgiPO?usp=share_link','_blank');
+}
